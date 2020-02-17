ca04ccb82d2a9091e65b99f89de33a1709f6966c
George Clark <clarkgeorge76@gmail.com>
2020-02-17 17:06:38 +0000

    tidy

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,15 +2,6 @@ import React, { useState } from 'react'
 import usePosts from '../hooks/usePosts'
 import { Link } from 'gatsby'
 
-// (
-//   description.toLowerCase().includes(query.toLowerCase()) ||
-//   title.toLowerCase().includes(query.toLowerCase()) ||
-//   tags
-//     .join("")
-//     .toLowerCase()
-//     .includes(query.toLowerCase())
-// )
-
 const PostList = () => {
   const allPosts = usePosts()
 
@@ -21,13 +12,10 @@ const PostList = () => {
   })
 
   const handleInputChange = (event, allPosts) => {
-    console.log(event.target.value)
     const query = event.target.value
 
     const posts = allPosts || []
-    console.log('all posts in handleChange', allPosts)
     const filteredData = posts.filter(post => {
-      console.log('💥', post);
       const { title, excerpt, tags } = post
       return (
         title.toLowerCase().includes(query.toLowerCase()) ||
@@ -48,10 +36,6 @@ const PostList = () => {
 
   return (
     <>
-      {console.log('hasSearchResults', hasSearchResults)}
-      {console.log('filteredData', filteredData)}
-      {console.log('allPosts', allPosts)}
-      {console.log('search', search)}
       <div className="searchBox">
         <input
           className="searchInput"
