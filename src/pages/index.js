5174dec9ca3154ff5dd3bcf503408b51b8d950a4
George Clark <clarkgeorge76@gmail.com>
2020-02-18 16:59:23 +0000

    amend

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,12 @@ const IndexPage = () => (
         <li>Styled Components</li>
         <li>Responsive Typography on a modular scale <a href="https://www.codementor.io/@ricardozea/100-responsive-typography-system-using-a-modular-scale-s5rhft58g" target='_blank' rel='noopener noreferrer'><strong>(ref)</strong></a> </li>
         <li>A reusable Image component you can pass an Image file to. <a href="https://spectrum.chat/gatsby-js/general/using-variables-in-a-staticquery~abee4d1d-6bc4-4202-afb2-38326d91bd05" target='_blank' rel='noopener noreferrer'><strong>(Deep Joy!)</strong></a></li>
-        <li>Fixed bottom footer</li>
+        <li>Fixed position bottom footer</li>
+        <ul>
+          <li><strong>TODO:</strong></li>
+          <li>Multilang</li>
+          <li>Theming</li>
+        </ul>
       </ul>
       <div style={{ maxWidth: `300px`, margin: `0 auto`, textAlign: `center` }}>
         <hr style={{ marginTop: `4rem` }} />
